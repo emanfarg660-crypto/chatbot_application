6ef5d59b96edd45e7948b12cf21703310679eabd
ImanIbrahim <emanfarg660@gmail.com>
2026-05-16 23:27:59 +0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -13,7 +13,7 @@ else:
 #function to generate text using google API
 def Generate_Text(text):
     #call generative model from google AI
-    model = genai.GenerativeModel('gemini-3-flash-preview')
+    model = genai.GenerativeModel('gemini-1.5-flash')
     #generate response from Gemini
     response = model.generate_content(text)
     return response.text
